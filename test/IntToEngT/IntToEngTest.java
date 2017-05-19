31b8fbb008c9aa5231a7fce0bfa5b0a0996f78d4
sae <sae@172.19.9.227>
2017-05-19 11:51:45 +0900

    ドライバ：Saaaaaaae ナビゲータ：isdMr

--- test/IntToEngT/IntToEngTest.java
+++ test/IntToEngT/IntToEngTest.java
@@ -1,15 +1,17 @@
 package IntToEngT;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class IntToEngTest {
-@Test
-public void 入力値1の時() {
-	//IntToEng ite = new IntToEng();
 
-fail("まだ実装されていません");
-	}
-
+		@Test
+		public void 入力値1のとき() {
+			IntToEng sn=new IntToEng();
+			String expected =" ";
+			String actual=sn.capitalize(" ");
+			assertThat(actual,is(expected));
+		}
 }
