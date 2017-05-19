e933285e1a7a6ccc9e19227bb76ba7fba31c3a94
3839101im <3839101im@172.19.14.232>
2017-05-19 11:23:52 +0900

    ドライバ:isdMr ナビゲーター:saaaaaaae

---
+++ src/IntToEng.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IntToEng {
+
+	// メインメソッド
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    int input = sc.nextInt();
+
+	    System.out.println(translateEng(input));
+
+	 }
+
+	  // 数値を英訳する変換するメソッド
+	static String translateEng(int n) {
+		if(n==1) return "one";
+		else if(n==2) return "two";
+		else if(n==3) return "three";
+		else if(n==4) return "four";
+		else if(n==5) return "five";
+		else if(n==6) return "six";
+		else if(n==7) return "seven";
+		else if(n==8) return "eight";
+		else if(n==9) return "nine";
+		else return "";
+	}
+}
---
+++ test/IntToEngT/IntToEngTest.java
@@ -0,0 +1,15 @@
+package IntToEngT;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntToEngTest {
+@Test
+public void 入力値1の時() {
+	//IntToEng ite = new IntToEng();
+
+fail("まだ実装されていません");
+	}
+
+}
