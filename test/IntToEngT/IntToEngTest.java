0c1494d0419b9e594e9043c7feadc5d96621ca84
sae <sae@172.19.15.209>
2017-05-24 12:54:02 +0900

    プログラムを変更しました。

--- test/IntToEngT/IntToEngTest.java
+++ test/IntToEngT/IntToEngTest.java
@@ -10,8 +10,29 @@ public class IntToEngTest {
 		@Test
 		public void 入力値1のとき() {
 			IntToEng sn=new IntToEng();
-			String expected =" ";
-			String actual=sn.capitalize(" ");
+			String expected ="one";
+			String actual=sn.translateEng(1);
+			assertThat(actual,is(expected));
+		}
+		@Test
+		public void 入力値21のとき() {
+			IntToEng sn=new IntToEng();
+			String expected ="twentyone";
+			String actual=sn.translateEng(21);
+			assertThat(actual,is(expected));
+		}
+		@Test
+		public void 入力値11のとき() {
+			IntToEng sn=new IntToEng();
+			String expected ="eleven";
+			String actual=sn.translateEng(11);
+			assertThat(actual,is(expected));
+		}
+		@Test
+		public void 入力値16のとき() {
+			IntToEng sn=new IntToEng();
+			String expected ="sixteen";
+			String actual=sn.translateEng(16);
 			assertThat(actual,is(expected));
 		}
 }
